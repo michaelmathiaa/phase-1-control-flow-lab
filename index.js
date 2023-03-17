76331f503b99bfc219d0622ba65e384d5163362a
Michael Mathias <michaelscodingjourney@gmail.com>
2023-03-17 10:43:53 -0700

    Passed 9 tests for control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let gForFeet;
+  if (distance <= 400) {
+    gForFeet = 'This one is on me!';
+    return gForFeet;
+  } else if (distance < 2000) {
+    gForFeet = 'That will be twenty bucks.';
+    return gForFeet;
+  } else if (distance < 2500) {
+    gForFeet = 'I will gladly take your thirty bucks.';
+    return gForFeet;
+  } else if (distance > 2500) {
+    gForFeet = 'No can do.';
+    return gForFeet;
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const cityCheck = "NYC";
+  const cityGreeting = cityCheck === city ? 'Ok, sounds good.' : 'No go.';
+  return cityGreeting;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipG){
   // Write your code here!
+  const tipGenerosity = tipG;
+  let response;
+  switch (tipGenerosity) {
+    case 'generous':
+      return response = 'Thank you so much.';
+    case 'not as generous':
+      return response = 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
